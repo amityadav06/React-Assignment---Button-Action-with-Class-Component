3658b13d54f451cc991d5730af792fc9308cf741
amityadav06 <55830008+amityadav06@users.noreply.github.com>
2021-02-16 17:28:55 +0530

    Update App.js
    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,30 @@ import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
-		super(props);
+	super(props);
+	    
+	    this.state = {
+	      	para: ""
 	};
+	    
+};
 
+	
+ handlePara = () => {
+	this.setState({
+		para: "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+	})
+}
+	
+	
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+			{ /* Do not remove this main div!! */ }
+			<button id="click" onClick={this.handlePara}>
+			  click
+			</button>
+			<h1 id="para">{this.state.para}</h1>
     		</div>
     	);
     }
